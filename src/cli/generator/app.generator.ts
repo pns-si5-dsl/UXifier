dfd321525ff8233ec4d0527a55c07c83754f2716
LudovicMarti <ludovic.marti@etu.unice.fr>
2022-02-16 11:28:02 +0100

    remove indents in app generator

--- src/cli/generator/app.generator.ts
+++ src/cli/generator/app.generator.ts
@@ -7,47 +7,33 @@ export function generateApplication(application: Application, node: CompositeGen
     const game: Context | undefined = application.games[0];
 
     node.append("export default function App() {",NL);
-    node.indent((appNode) => {
-        appNode.append("return (",NL)
-        appNode.indent(appNode1 => {
-            appNode1.append("<BrowserRouter>",NL)
-            appNode1.indent(appNode2 => {
-                appNode2.append("<Grommet>",NL)
-                appNode2.indent(appNode3 => {
-                    appNode3.append("<Routes>",NL)
-                    appNode3.indent(appNode4 => {
-                        if(config) {
-                            appNode4.append('<Route path="/', config.name,'" element={<', config.name,'/>}>',NL)
-                            appNode4.indent(configNode => {
-                                config.pages.forEach(page => {
-                                    configNode.append(
-                                        '<Route path="',
-                                        '/',  config.name,'/',page.name,
-                                        '" element={<', config.name,'><',page.name,'/></', config.name,'>}/>',NL)
-                                })
-                            })
-                            appNode4.append('</Route>',NL)
-                        }
-                        if(game) {
-                            appNode4.append('<Route path="/', game.name,'" element={<', game.name,'/>}>',NL)
-                            appNode4.indent(gameNode => {
-                                game.pages.forEach(page => {
-                                    gameNode.append(
-                                        '<Route path="',
-                                        '/',  game.name,'/',page.name,
-                                        '" element={<', game.name,'><',page.name,'/></', game.name,'>}/>',NL)
-                                })
-                            })
-                            appNode4.append('</Route>',NL)
-                        }
-                    })
-                    appNode3.append("</Routes>",NL)
-                })
-                appNode2.append("</Grommet>",NL)
-            })
-            appNode1.append("</BrowserRouter>",NL)
+    node.append("return (", NL);
+    node.append("<BrowserRouter>", NL);
+    node.append("<Grommet>", NL);
+    node.append("<Routes>", NL);
+    if(config) {
+        node.append('<Route path="/', config.name,'" element={<', config.name,'/>}>', NL);
+        config.pages.forEach(page => {
+            node.append(
+                '<Route path="',
+                '/',  config.name,'/',page.name,
+                '" element={<', config.name,'><',page.name,'/></', config.name,'>}/>',NL)
+        });
+        node.append('</Route>', NL);
+    }
+    if(game) {
+        node.append('<Route path="/', game.name,'" element={<', game.name,'/>}>',NL)
+        game.pages.forEach(page => {
+            node.append(
+                '<Route path="',
+                '/',  game.name,'/',page.name,
+                '" element={<', game.name,'><',page.name,'/></', game.name,'>}/>',NL)
         })
-        appNode.append(")",NL,NL)
-    })
-    node.append("}",NL,NL)
+        node.append('</Route>',NL)
+    }
+    node.append("</Routes>", NL);
+    node.append("</Grommet>", NL);
+    node.append("</BrowserRouter>", NL);
+    node.append(")", NL);
+    node.append("}", NL);
 }
