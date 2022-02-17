ff4ea8c457ffe3c0a572fe3d2b8c84cd4a8a031c
LudovicMarti <ludovic.marti@etu.unice.fr>
2022-02-17 12:41:34 +0100

    customise syntax highlighting and try vscode.workspace.FileSystemWatcher

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -5,11 +5,20 @@ import { Application } from '../language-server/generated/ast';
 import { createUxifierServices } from '../language-server/uxifier-module';
 import { extractAstNode } from './cli-util';
 import { generateProject } from './generator';
+// import { window, workspace } from 'vscode';
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     const model = await extractAstNode<Application>(fileName, languageMetaData.fileExtensions, createUxifierServices());
     const generatedFilePath = generateProject(model, fileName, opts.destination);
     console.log(colors.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+};
+
+export const watchAction = async (): Promise<void> => {
+    // const languageExt = require('../../package.json').contributes.languages[0].extensions[0];
+    // const watcher = workspace.createFileSystemWatcher('**/*' + languageExt);
+    // watcher.onDidChange(() => {
+    //      window.showInformationMessage('Your web app was updated!');
+    // });
 };
 
 export type GenerateOptions = {
@@ -27,8 +36,13 @@ export default function(): void {
         .command('generate')
         .argument('<file>', `possible file extensions: ${languageMetaData.fileExtensions.join(', ')}`)
         .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
+        .description('generates a web app of an RPG character sheet')
         .action(generateAction);
+
+    program
+        .command('watch')
+        .description('watches for project files save and generate the web app')
+        .action(watchAction);
 
     program.parse(process.argv);
 }
